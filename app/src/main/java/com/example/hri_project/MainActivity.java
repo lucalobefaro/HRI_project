54e7bb76dace047aed2899bbb2fe674d017d0343
lucalobefaro <lobefaro.1890195@studenti.uniroma1.it>
2021-05-19 10:22:37 +0200

    Main Class structure

--- app/src/main/java/com/example/hri_project/MainActivity.java
+++ app/src/main/java/com/example/hri_project/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.hri_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
+import com.aldebaran.qi.Future;
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
-import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.builder.ChatBuilder;
+import com.aldebaran.qi.sdk.builder.QiChatbotBuilder;
+import com.aldebaran.qi.sdk.builder.TopicBuilder;
 import com.aldebaran.qi.sdk.design.activity.RobotActivity;
-import com.aldebaran.qi.sdk.object.conversation.Say;
+import com.aldebaran.qi.sdk.object.conversation.Chat;
+import com.aldebaran.qi.sdk.object.conversation.QiChatbot;
+import com.aldebaran.qi.sdk.object.conversation.Topic;
 
 
 public class MainActivity extends RobotActivity implements RobotLifecycleCallbacks {
+
+    private Chat greetingsChat;
+    private QiChatbot qiGreetingsChatbot;
+
+    private boolean testLevel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +40,66 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
 
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
-        // The robot focus is gained.
-        Say say = SayBuilder.with(qiContext) // Create the builder with the context.
-                .withText("Hello human!") // Set the text to say.
-                .build(); // Build the say action.
 
-        // Execute the action.
-        say.run();
+        // Initialize all the chats
+        createGreetingsChat(qiContext);
+
+
+        // Run the greetings chat
+        startGreetingsChat();
+
     }
 
     @Override
     public void onRobotFocusLost() {
-        // The robot focus is lost.
+        // Remove on started listeners from the Chat action.
+        greetingsChat.removeAllOnStartedListeners();
     }
 
     @Override
     public void onRobotFocusRefused(String reason) {
         // The robot focus is refused.
+    }
+
+    private void createGreetingsChat(QiContext qiContext) {
+        // The robot focus is gained.
+        Topic topic = TopicBuilder.with(qiContext)      // Create the builder using the qiContext
+                .withResource(R.raw.greetings)  // Set the topic resource.
+                .build();                       // Build the topic
+
+        // Create a new QiChatbot.
+        qiGreetingsChatbot = QiChatbotBuilder.with(qiContext)
+                .withTopic(topic)
+                .build();
+
+        // Create a new Chat action.
+        greetingsChat = ChatBuilder.with(qiContext)
+                .withChatbot(qiGreetingsChatbot)
+                .build();
+
+        // Add an on started listener to the Chat action.
+        greetingsChat.addOnStartedListener(() -> Log.i("TAG", "Discussion started."));
+    }
+
+
+    private void startGreetingsChat() {
+        Future<Void> chatFuture = greetingsChat.async().run();
+
+        // Add a Lambda to the action execution.
+        chatFuture.thenConsume(future -> {
+            if (future.hasError()) {
+                Log.e("TAG", "Discussion finished with error.", future.getError());
+            }
+        });
+
+        // Stop the chat when the qichatbot is done
+        qiGreetingsChatbot.addOnEndedListener(endReason -> {
+            testLevel = endReason.equals("test");
+            Log.i("TAG", "" + testLevel);
+
+            // Start test or choose basing on testLevel
+
+        });
     }
 
 }
